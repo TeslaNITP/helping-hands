f76214a03f1b4067e140f4a63eabeae353473103
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-01-08 14:39:50 +0530

    finished subject listing

--- app.js
+++ app.js
@@ -10,124 +10,76 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/frontend", (req, res) => {
-  res.render("frontend");
+app.get("/engineeringchemistry", (req, res) => {
+  res.render("materials/engineeringchemistry");
 });
 
-app.get("/backend", (req, res) => {
-  res.render("backend");
+app.get("/em2", (req, res) => {
+  res.render("materials/em2");
 });
 
-app.get("/angular", (req, res) => {
-  res.render("materials/angular");
+app.get("/ee2", (req, res) => {
+  res.render("materials/ee2");
 });
 
-app.get("/android", (req, res) => {
-  res.render("materials/android");
+app.get("/ed", (req, res) => {
+  res.render("materials/ed");
 });
 
-app.get("/bootstrap", (req, res) => {
-  res.render("materials/bootstrap");
+
+app.get("/ec1", (req, res) => {
+  res.render("materials/ec1");
 });
 
-app.get("/c", (req, res) => {
-  res.render("materials/c");
+app.get("/ico", (req, res) => {
+  res.render("materials/ico");
 });
 
-app.get("/cpp", (req, res) => {
-  res.render("materials/cpp");
+app.get("/ee1", (req, res) => {
+  res.render("materials/ee1");
 });
 
-app.get("/csharp", (req, res) => {
-  res.render("materials/c#");
+app.get("/electricalworkshop", (req, res) => {
+  res.render("materials/electricalworkshop");
 });
 
-app.get("/datastructure", (req, res) => {
-  res.render("materials/datastructure");
+app.get("/eaa-sports", (req, res) => {
+  res.render("materials/eaa-sports");
 });
 
-app.get("/deepLearning", (req, res) => {
-  res.render("materials/deepLearning");
+app.get("/eaa-sbm", (req, res) => {
+  res.render("materials/eaa-sbm");
 });
 
-app.get("/django", (req, res) => {
-  res.render("materials/django");
+app.get("/enggphy", (req, res) => {
+  res.render("materials/enggphy");
 });
 
-app.get("/git", (req, res) => {
-  res.render("materials/git");
+
+app.get("/communicativeenglish", (req, res) => {
+  res.render("materials/communicativeenglish");
 });
 
-app.get("/go", (req, res) => {
-  res.render("materials/go");
+app.get("/engineeringmechanics", (req, res) => {
+  res.render("materials/engineeringmechanics");
 });
 
-app.get("/haskell", (req, res) => {
-  res.render("materials/haskell");
+app.get("/workshoppractice", (req, res) => {
+  res.render("materials/workshoppractice");
 });
 
-app.get("/htmlcss", (req, res) => {
-  res.render("materials/htmlcss");
-});
 
-app.get("/javascript", (req, res) => {
-  res.render("materials/javascript");
-});
-
-app.get("/machinelearning", (req, res) => {
-  res.render("materials/machinelearning");
-});
-
-app.get("/matlab", (req, res) => {
-  res.render("materials/matlab");
-});
-
-app.get("/mongodb", (req, res) => {
-  res.render("materials/mongodb");
-});
-
-app.get("/nodejs", (req, res) => {
-  res.render("materials/nodejs");
-});
-
-app.get("/php", (req, res) => {
-  res.render("materials/php");
-});
-
-app.get("/prolog", (req, res) => {
-  res.render("materials/prolog");
+app.get("/ssev", (req, res) => {
+  res.render("materials/ssev");
 });
 
 app.get("/maths", (req, res) => {
   res.render("materials/maths");
 });
 
-app.get("/react", (req, res) => {
-  res.render("materials/react");
-});
 
-app.get("/redux", (req, res) => {
-  res.render("materials/redux");
-});
-
-app.get("/rlanguage", (req, res) => {
-  res.render("materials/rlanguage");
-});
-
-app.get("/ruby", (req, res) => {
-  res.render("materials/ruby");
-});
-
-app.get("/scala", (req, res) => {
-  res.render("scala");
-});
-
-app.get("/vue", (req, res) => {
-  res.render("materials/vue");
-});
-
-app.get("/webaccessibility", (req, res) => {
-  res.render("materials/webaccessibility");
+app.get("/electronicsworkshop", (req, res) => {
+  res.render("materials/electronicsworkshop");
 });
 
 app.listen(5050, function () {
